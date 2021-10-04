0532e37bd1948992b41e58c927958f64c04e293e
AceWestt <asilbekkhamzaev@gmail.com>
2021-10-04 13:56:25 +0500

    add certificate for mongodb

--- config/db.js
+++ config/db.js
@@ -1,7 +1,21 @@
+const username = "doadmin";
+const password = "oir07f456E29uDP3";
+const dbName = "admin";
+const host = "private-db-mongodb-fra1-23365-bd71d177.mongo.ondigitalocean.com";
+const port = "27017";
+
+const mongodbProdUri = `mongodb+srv://${username}:${password}@${host}:${port}/${dbName}`;
+
+const options = {
+  ssl: true,
+  sslValidate: true,
+  sslCA: `${__dirname}/ca-certificate.crt.txt`,
+};
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI || mongodbProdUri);
 
   console.log("MongoDB connected");
 };
