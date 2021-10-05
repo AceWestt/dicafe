e62cfe0b7e6fb1aacb6cfc72b8ad6c8aaedd577f
AceWestt <asilbekkhamzaev@gmail.com>
2021-10-05 17:19:29 +0500

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "dicafe",
+      script: "./server.js",
+      watch: true,
+      env_production: {
+        JWT_SECRET:
+          "be6f4f02b627e93e93de964c2785fafa803979e0e2870c724c50f14c8f4725a375e904",
+        JWT_EXPIRE: "2d",
+      },
+    },
+  ],
+};
