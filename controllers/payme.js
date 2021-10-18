05f990bb4943b98abb5dcaffbf320140006be94e
AceWestt <asilbekkhamzaev@gmail.com>
2021-10-18 17:15:13 +0500

    minor changes

--- controllers/payme.js
+++ controllers/payme.js
@@ -3,16 +3,14 @@ const { JSONRPCServer } = require('json-rpc-2.0');
 
 const rpcServer = new JSONRPCServer();
 
-exports.payme = async (req, res, next) => {
+exports.payme = (req, res, next) => {
 	const jsonRPCRequest = req.body;
 
 	rpcServer.receive(jsonRPCRequest).then((jsonRPcResponse) => {
 		if (jsonRPcResponse) {
 			res.json(jsonRPcResponse);
 		} else {
-			res.status(204);
+			res.sendStatus(204);
 		}
 	});
-
-	res.status(200).json({ data: 'payme controller' });
 };
