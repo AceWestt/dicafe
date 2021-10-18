e447f45ec68d2e30313ff929980f403fb878e55f
AceWestt <asilbekkhamzaev@gmail.com>
2021-10-18 17:11:09 +0500

    add json_rpc config

--- controllers/payme.js
+++ controllers/payme.js
@@ -1,7 +1,18 @@
 const ErrorResponse = require('../utils/errorResponse');
+const { JSONRPCServer } = require('json-rpc-2.0');
+
+const rpcServer = new JSONRPCServer();
 
 exports.payme = async (req, res, next) => {
-	const body = req.body;
-	console.log(body);
+	const jsonRPCRequest = req.body;
+
+	rpcServer.receive(jsonRPCRequest).then((jsonRPcResponse) => {
+		if (jsonRPcResponse) {
+			res.json(jsonRPcResponse);
+		} else {
+			res.status(204);
+		}
+	});
+
 	res.status(200).json({ data: 'payme controller' });
 };
