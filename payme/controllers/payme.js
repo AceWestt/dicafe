22a91fa3241d3ccbd4a978f95db6d54439267bce
AceWestt <asilbekkhamzaev@gmail.com>
2021-10-20 11:15:45 +0500

    another commit

--- payme/controllers/payme.js
+++ payme/controllers/payme.js
@@ -123,6 +123,8 @@ exports.paymeAnother = (req, res, next) => {
 
 	// const isauthorized = merchant.authorize(req.headers);
 	res.json({
+		jsonrpc: '2.0',
+		id: reqId,
 		error: {
 			code: -31001,
 			message: 'hello',
