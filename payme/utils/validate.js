75a0018d6f6e13b64a5cacc3cdb30849222e35d0
AceWestt <asilbekkhamzaev@gmail.com>
2021-10-21 04:47:57 +0500

    something important

--- payme/utils/validate.js
+++ payme/utils/validate.js
@@ -14,6 +14,11 @@ const validate = (params) => {
 	} else {
 		if (!params.account.order_id && !params.account.DiCafe) {
 			return respond(false, ERROR_INVALID_ACCOUNT);
+		} else {
+			const order_id = params.account.order_id || params.account.DiCafe;
+			if (!order_id.match(/^[0-9a-fA-F]{24}$/)) {
+				return respond(false, ERROR_INVALID_ACCOUNT);
+			}
 		}
 	}
 	return respond(true);
