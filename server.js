0e9c56997e7a0b3fa4d6ceaf68bf6214e5082063
AceWestt <asilbekkhamzaev@gmail.com>
2021-10-20 09:51:38 +0500

    commit

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.get(/^\/(?!api).*$/, function (req, res) {
 	res.sendFile(path.join(`${__dirname}/client`, 'build', 'index.html'));
 });
 
-app.use(express.json());
 app.use(jsonrpc());
 app.use(fileUpload());
 
