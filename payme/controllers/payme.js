6320ccdf0d34d8fd5d91632a0103a7c7afd136cc
AceWestt <asilbekkhamzaev@gmail.com>
2021-10-20 11:42:16 +0500

    another commit

--- payme/controllers/payme.js
+++ payme/controllers/payme.js
@@ -1,8 +1,16 @@
 const Merchant = require('../models/Merchant');
 const config = require('../config/config');
+const validate = require('../utils/validate');
 
+const JSON_RPC_VERSION = '2.0';
 const ERROR_INSUFFICIENT_PRIVILEGE = -32504;
 const ERROR_INVALID_AMOUNT = -31001;
+const ERROR_METHOD_NOT_FOUND = -32601;
+
+const ERROR_INSUFFICIENT_PRIVILEGE_MSG =
+	'Insufficient privilege to perform this method.';
+const ERROR_INVALID_AMOUNT_MSG = 'Incorrect amount!';
+const ERROR_METHOD_NOT_FOUND_MSG = 'Method not found!';
 
 exports.payme = (req, res, next) => {
 	// const userID = getUserID(req);
@@ -117,8 +125,9 @@ exports.paymeAnother = (req, res, next) => {
 	const rpcRequest = req.body;
 	const headers = req.headers;
 	const reqId = rpcRequest.id;
+	const method = rpcRequest.method;
+	const params = rpcRequest.params;
 
-	console.log(headers);
 	const merchant = new Merchant(config);
 
 	const isauthorized = merchant.authorize(req.headers);
@@ -128,16 +137,41 @@ exports.paymeAnother = (req, res, next) => {
 			id: reqId,
 			error: {
 				code: ERROR_INSUFFICIENT_PRIVILEGE,
-				message: 'Insufficient privilege to perform this method.',
+				message: ERROR_INSUFFICIENT_PRIVILEGE_MSG,
 			},
 		});
 	}
+	switch (method) {
+		case 'CheckPerformTransaction':
+			CheckPerformTransaction(res, reqId, params);
+			break;
+		default:
+			res.json({
+				jsonrpc: JSON_RPC_VERSION,
+				id: reqId,
+				error: {
+					code: ERROR_METHOD_NOT_FOUND,
+					message: ERROR_METHOD_NOT_FOUND_MSG,
+				},
+			});
+	}
 	res.json({
-		jsonrpc: '2.0',
+		jsonrpc: JSON_RPC_VERSION,
 		id: reqId,
 		error: {
-			code: -31001,
-			message: 'hello',
+			code: ERROR_METHOD_NOT_FOUND,
+			message: ERROR_METHOD_NOT_FOUND_MSG,
+		},
+	});
+};
+
+const CheckPerformTransaction = (res, reqid, params) => {
+	res.json({
+		jsonrpc: JSON_RPC_VERSION,
+		id: reqid,
+		error: {
+			code: ERROR_INVALID_AMOUNT,
+			message: ERROR_INVALID_AMOUNT_MSG,
 		},
 	});
 };
